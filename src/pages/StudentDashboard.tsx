3261404a3dd5ede66af8104c39b650c2af3e2927
Divine-Ramiel <ramieldivine@gmail.com>
2025-09-10 16:48:00 +0100

    schoollist3

--- src/pages/StudentDashboard.tsx
+++ src/pages/StudentDashboard.tsx
@@ -21,7 +21,7 @@ function StudentDashboard() {
         <strong>Class:</strong> {student.class || student.className}<br/>
         <strong>Address:</strong> {student.address}
       </div>
-      <a href="/students/list" style={{
+      <a href="/students" style={{
         display:'inline-block',
         marginTop:'2rem',
         background:'#003366',
